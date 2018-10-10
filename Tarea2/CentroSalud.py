9c6ad5d515303d3d29223b8c191a1de590700447
Diego Varas <davarasm@gmail.com>
2018-10-10 14:02:27 -0300

    Termino de funciones
    se termino la funcion de agregar paciente y se completo el main

--- Tarea2/CentroSalud.py
+++ Tarea2/CentroSalud.py
@@ -1,52 +1,63 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 import numpy as np
 import pandas as pd
 import time
 import collections
 import os
+import random
 
 class CentroSalud:
     def __init__(self,nombre_archivo):
         self.nombre_archivo = nombre_archivo
         self.datos = pd.read_csv(self.nombre_archivo,sep=';')
+        
+    def nombre_centro(self):
+        return str(self.nombre_archivo.rstrip('.csv'))
 
     def estadisticas_especialidad (self,especialidad,parametro):
         p = np.asarray(self.datos.set_index('especialidad').loc[especialidad,parametro])
-        desviacion = p.std()
-        promedio = p.mean()
-        return str(int(promedio))+"+-"+str(int(desviacion))       
+        desviacion = int(p.std())
+        promedio = round(float(p.mean()),2)
+        return str(promedio)+"+-"+str(desviacion)       
         
     def especialidad_imc (self,especialidad):
         m = np.asarray(self.datos.set_index('especialidad').loc[especialidad,"masa"])
         a = np.asarray(self.datos.set_index('especialidad').loc[especialidad,"altura"])
-        IMC = []
-        obeso=0
+        IMC,obeso = [],0
         for i in range(len(m)):
            IMC.append((float(m[i])/((float(a[i])/100.0)**2)))
            if(IMC[i]>=30):
                obeso+=1
         imc_a = np.array(IMC)
-        promedio = imc_a.mean()
-        desviacion = imc_a.std()
-        print "Promedio IMC: "+str(int(promedio))+"+-"+str(int(desviacion))+" Pacientes con obesidad: "+str(obeso)        
+        promedio = round(float(imc_a.mean()),2)
+        desviacion = int(imc_a.std())
+        print "Promedio IMC: "+str(promedio)+"+-"+str(desviacion)+" Pacientes con obesidad: "+str(obeso)        
         
     def especialidad_pam (self,especialidad):
         pas = np.asarray(self.datos.set_index('especialidad').loc[especialidad,"pas"])
         pad = np.asarray(self.datos.set_index('especialidad').loc[especialidad,"pad"])
-        PAM = []
-        normal=0
+        PAM,normal = [],0
         for i in range(len(pas)):
            PAM.append(((float(2*pad[i]))+float(pas[i]))/3)
            if(PAM[i]>=70 and PAM[i]<=105):
                normal+=1
         pam_a = np.array(PAM)
-        promedio = pam_a.mean()
-        desviacion = pam_a.std()
-        print "Promedio PAM: "+str(int(promedio))+"+-"+str(int(desviacion))+" Pacientes con un rango normal: "+str(normal)
+        promedio = round(float(pam_a.mean()),2)
+        desviacion = int(pam_a.std())
+        print "Promedio PAM: "+str(promedio)+"+-"+str(desviacion)+" Pacientes con un rango normal: "+str(normal)
     
     def nuevo_paciente(self):
-        return None
+        id = int(random.randrange(min(self.datos['ID']),max(self.datos['ID'])))       
+        new = pd.DataFrame(columns=['ID','especialidad','masa','altura','pas','pad'])
+        esp = raw_input("Especialidad: ")
+        masa = int(input("Masa: "))
+        altura = int(input("Altura: "))
+        pas = int(input("PAS: "))
+        pad = int(input("PAD: "))
+        new = new.append([{'ID':id,'especialidad':esp,'masa':masa,'altura':altura,'pas':pas,'pad':pad}])
+        self.datos = pd.concat([self.datos,new],ignore_index=True)
+        print 'Paciente Agregado'
         
     def guardar_datos(self,nombre_archivo_csv):
         self.datos.set_index('ID').to_csv(nombre_archivo_csv,sep=str(';'))        
@@ -67,7 +78,7 @@ class CentroSalud:
             file.write("Pas: "+self.estadisticas_especialidad(esp[i],"pas")+" mmHg\n")
             file.write("Pad: "+self.estadisticas_especialidad(esp[i],"pad")+" mmHg\n\n") 
         file.close()
-        os.system(str(self.nombre_archivo.rstrip('.csv'))+"_"+str(time.strftime("%d_%m_%y"))+".txt")
+        #os.system(str(self.nombre_archivo.rstrip('.csv'))+"_"+str(time.strftime("%d_%m_%y"))+".txt")
         return None
     
     def __add__(self,otro):
--- Tarea2/main.py
+++ Tarea2/main.py
@@ -2,20 +2,56 @@
 from __future__ import unicode_literals
 import CentroSalud as cs
 
+esp_a = ['broncopulmonar', 'cirugia', 'endocrinologia', 'neurologia', 'cardiologia adulto'
+]
+esp_b = ['cirugia', 'urologia', 'endocrinologia', 'cardiologia adulto', 'nefrologia', 'broncopulmonar'
+]
+esp_ab = esp_a + esp_b
+para = ['masa','altura','pas','pad']
+
 c1 = cs.CentroSalud("centro_A.csv")
 c2 = cs.CentroSalud("centro_B.csv")
 
-print c1.estadisticas_especialidad("cirugia","masa")
-c1.especialidad_imc("cirugia")
-c1.especialidad_pam("cirugia")
-c1.especialidad_imc("cardiologia adulto")
-c1.especialidad_pam("cardiologia adulto")
-c1.guardar_datos("hoa.csv")
-c2.especialidad_imc("cardiologia adulto")
-c1.reporte()
+print '\n-------------'+c1.nombre_centro()+'-------------\n'
+for i in esp_a:
+    print '***'+i+': '
+    for j in para:
+        print j+'= '+c1.estadisticas_especialidad(i,j)
+    c1.especialidad_imc(i)
+    c1.especialidad_pam(i)
+    print'\n'
+    
+#c1.nuevo_paciente()
+#c1.reporte()
+#c1.guardar_datos("newDataCentroA.csv")
+print '---------------------------------------'
 
-c3 = c1 +c2
+print '\n-------------'+c2.nombre_centro()+'-------------\n'
+for i in esp_b:
+    print '***'+i+': '
+    for j in para:
+        print j+'= '+c2.estadisticas_especialidad(i,j)
+    c2.especialidad_imc(i)
+    c2.especialidad_pam(i)
+    print'\n'
 
-c3.especialidad_imc("cardiologia adulto")
-print c3.estadisticas_especialidad("cardiologia adulto","masa")
+#c2.nuevo_paciente()
+#c2.reporte()
+#c2.guardar_datos("newDataCentroB.csv")
+print '---------------------------------------'
+
+c3 = c1 + c2 #Sumar Centros
+
+print '\n-------------'+c3.nombre_centro()+'-------------\n'
+for i in esp_ab:
+    print '***'+i+': '
+    for j in para:
+        print j+'= '+c3.estadisticas_especialidad(i,j)
+    c3.especialidad_imc(i)
+    c3.especialidad_pam(i)
+    print'\n'
+
+#c3.nuevo_paciente()
 #c3.reporte()
+#c3.guardar_datos("newDataCentroAB.csv")
+print '---------------------------------------'
