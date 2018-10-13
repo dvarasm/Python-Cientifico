240ff627d095ab6c0278c9235c5a8bd15ad5bec1
Diego Varas <davarasm@gmail.com>
2018-10-13 14:38:33 -0300

    Agrega Comentarios a Main

--- Tarea2/main.py
+++ Tarea2/main.py
@@ -2,54 +2,55 @@
 from __future__ import unicode_literals
 import CentroSalud as cs
 
-c1 = cs.CentroSalud("centro_A.csv")
-c2 = cs.CentroSalud("centro_B.csv")
+#main para mostrar todas las funcionalidades de la Clase CentroSalud()
 
-esp_a = c1.especialidades_centro()
-esp_b = c2.especialidades_centro()
-esp_ab = esp_a + esp_b
-para = ['masa','altura','pas','pad']
+c1 = cs.CentroSalud("centro_A.csv")#crea objeto centro 1
+c2 = cs.CentroSalud("centro_B.csv")#crea objeto centro 2
+c3 = c1 + c2 #Sumar Objetos CentroSalud
 
-print '\n-------------'+c1.nombre_centro()+'-------------\n'
+esp_a = c1.especialidades_centro()#metodo que obtiene todas las especialidades del centro
+esp_b = c2.especialidades_centro()#metodo que obtiene todas las especialidades del centro
+esp_ab = esp_a + esp_b #suma las especialidades para el centro c3
+para = ['masa','altura','pas','pad'] #parametros
+
+print '\n-------------'+c1.nombre_centro()+'-------------\n'#imprimir todas las estadisticas del centro
 for i in esp_a:
-    print '***'+i+': '
+    print '***'+i+': '#imprime nombre de la especialidad
     for j in para:
-        print j+'= '+c1.estadisticas_especialidad(i,j)
-    c1.especialidad_imc(i)
-    c1.especialidad_pam(i)
+        print j+'= '+c1.estadisticas_especialidad(i,j)#imprime estadistica por especialidad
+    c1.especialidad_imc(i)#imprime IMC por especialidad
+    c1.especialidad_pam(i)#imprime PAM por especialidad
     print'\n'
     
-#c1.nuevo_paciente()
-c1.reporte()
-c1.guardar_datos("newDataCentroA.csv")
+#c1.nuevo_paciente()#crea un paciente nuevo
+c1.reporte()#crea el reporte
+c1.guardar_datos("newDataCentroA.csv")#guarda los datos en un archivo csv
 print '---------------------------------------'
 
-print '\n-------------'+c2.nombre_centro()+'-------------\n'
+print '\n-------------'+c2.nombre_centro()+'-------------\n'#imprimir todas las estadisticas del centro
 for i in esp_b:
-    print '***'+i+': '
+    print '***'+i+': '#imprime nombre de la especialidad
     for j in para:
-        print j+'= '+c2.estadisticas_especialidad(i,j)
-    c2.especialidad_imc(i)
-    c2.especialidad_pam(i)
+        print j+'= '+c2.estadisticas_especialidad(i,j)#imprime estadistica por especialidad
+    c2.especialidad_imc(i)#imprime IMC por especialidad
+    c2.especialidad_pam(i)#imprime PAM por especialidad
     print'\n'
 
-#c2.nuevo_paciente()
-c2.reporte()
-c2.guardar_datos("newDataCentroB.csv")
+#c2.nuevo_paciente()#crea un paciente nuevo
+c2.reporte()#crea el reporte
+c2.guardar_datos("newDataCentroB.csv")#guarda los datos en un archivo csv
 print '---------------------------------------'
 
-c3 = c1 + c2 #Sumar Centros
-
-print '\n-------------'+c3.nombre_centro()+'-------------\n'
+print '\n-------------'+c3.nombre_centro()+'-------------\n'#imprimir todas las estadisticas del centro
 for i in esp_ab:
-    print '***'+i+': '
+    print '***'+i+': '#imprime nombre de la especialidad
     for j in para:
-        print j+'= '+c3.estadisticas_especialidad(i,j)
-    c3.especialidad_imc(i)
-    c3.especialidad_pam(i)
+        print j+'= '+c3.estadisticas_especialidad(i,j)#imprime estadistica por especialidad
+    c3.especialidad_imc(i)#imprime IMC por especialidad
+    c3.especialidad_pam(i)#imprime PAM por especialidad
     print'\n'
 
-#c3.nuevo_paciente()
-c3.reporte()
-c3.guardar_datos("newDataCentroAB.csv")
+#c3.nuevo_paciente()#crea un paciente nuevo
+c3.reporte()#crea el reporte
+c3.guardar_datos("newDataCentroAB.csv")#guarda los datos en un archivo csv
 print '---------------------------------------'
