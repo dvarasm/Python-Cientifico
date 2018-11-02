e61a15becbfbb11ff186d856d2b896d4a0ab594c
Diego Varas <davarasm@gmail.com>
2018-11-02 17:31:49 -0300

    Mejoras
    Ordenamiento de los botones y agrega soporte para poner imagenes

--- Proyecto/interfaz.py
+++ Proyecto/interfaz.py
@@ -1,24 +1,27 @@
-import Tkinter as Tk
+from Tkinter import *
+from PIL import ImageTk, Image
+import os
 
-v = Tk.Tk()
+v = Tk()#ventana de tipo tkinter
 v.title('Detector')#titulo de ventana
-v.geometry('800x600')#tamano de ventana
+v.geometry('800x400')#tamano de ventana
+v.resizable(width=False, height=False)#la ventana no se puede cambiar de tamano
 
 def funcion_b1():#funcion del boton 1
-    
     print "Iniciar"
 
 def funcion_b2():#funcion del boton 2
     print "Parar"
 
-numero = 8
+numero = 8 #variable a obtener
 var_sos = "Elementos\nsospechosos: "+str(numero)
 
-boton1 = Tk.Button(v,text="Iniciar Deteccion",command=funcion_b1,width=13, height=1,bg='green')
-boton1.grid(row=1,column=1)#posicion boton 1
-boton2 = Tk.Button(v,text="Parar Deteccion",command=funcion_b2,width=13, height=1,bg='red')
-boton2.grid(row=2,column=1)#posicion boton 2
-texto = Tk.Label(v,text=var_sos,bg='grey')
-texto.grid(row=4,column=1)
-
+#contenido de la ventana
+boton1 = Button(v,text="Iniciar Deteccion",command=funcion_b1,width=13, height=1,bg='green',foreground="white").place(x=20,y=30)
+boton2 = Button(v,text="Parar Deteccion",command=funcion_b2,width=13, height=1,bg='red',foreground="white").place(x=20,y=60)
+texto = Label(v,text=var_sos,bg='grey',justify="center",width=13).place(x=20,y=350)
+image = Image.open("ma.png")
+image = image.resize((620, 350), Image.ANTIALIAS)#redimensionar imagen
+img = ImageTk.PhotoImage(image)
+panel = Label(v, image = img,width=620, height=350).place(x=150,y=30)#panel que contiene la imagen
 v.mainloop()#para qe se vea la ventana
