4a9ac99a7e2158a4eaf12d9644633a8b783a95f3
konata2117 <natashasmile11@gmail.com>
2018-11-11 23:39:37 -0300

    Cosa

--- Proyecto/prueba.py
+++ Proyecto/prueba.py
@@ -2,26 +2,41 @@ import numpy as np
 import cv2
  
 # Cargamos la imagen
-original = cv2.imread("apple.jpg")
+original = cv2.imread("manzana5.jpg")
 cv2.imshow("original", original)
- 
-# Convertimos a escala de grises
+original =cv2.medianBlur(original,5)
 gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+ret,th1 = cv2.threshold(gris,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(gris,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(gris,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+kernel = np.ones((3,3),np.uint8)
+
+trans= cv2.morphologyEx(th3,cv2.MORPH_CLOSE,kernel)
+
+cv2.imshow("th1", th1)
+cv2.imshow("th2", th2)
+cv2.imshow("th3", th3)
+cv2.imshow("trans", trans)
+
+# Convertimos a escala de grises
+#gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
  
 # Aplicar suavizado Gaussiano
-gauss = cv2.GaussianBlur(gris, (5,5), 0)
+#gauss = cv2.GaussianBlur(gris, (5,5), 0)
  
-cv2.imshow("suavizado", gauss)
+#cv2.imshow("suavizado", gauss)
  
 # Detectamos los bordes con Canny
-canny = cv2.Canny(gauss, 50, 150)
+canny = cv2.Canny(trans, 50, 150)
  
 cv2.imshow("canny", canny)
  
 # Buscamos los contornos
-(, contornos,) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+( contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
  
-#Mostramos el numero de monedas por consola
+# Mostramos el numero de monedas por consola
 print "He encontrado {} objetos".format(len(contornos))
  
 cv2.drawContours(original,contornos,-1,(0,0,255), 2)
