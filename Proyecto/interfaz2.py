ac49dfbf4241e272045eea321fae5370e1399f0c
Diego Varas <davarasm@gmail.com>
2018-11-13 18:31:48 -0300

    Mejoras Varias

--- Proyecto/interfaz2.py
+++ Proyecto/interfaz2.py
@@ -10,7 +10,7 @@ class App(QtWidgets.QApplication):
         self.MainWindow.ui.setWindowTitle("Detector")#nombre de la ventana
         self.MainWindow.ui.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint|QtCore.Qt.WindowCloseButtonHint)#para no redimensionar
         self.MainWindow.ui.show()
-        self.fileChosen = None
+        #self.fileChosen = None
         self.path = ''
                 
         #self.connect(self.MainWindow.ui.saludar, QtCore.SIGNAL('clicked()'), self.saludar)#ejemplo pyqt4
@@ -18,7 +18,7 @@ class App(QtWidgets.QApplication):
         
         #botones
         self.MainWindow.ui.Iniciar.clicked.connect(self.funcion_b1)#listener del boton iniciar
-        self.MainWindow.ui.Detener.clicked.connect(self.funcion_b2)#listener del boton parar
+        self.MainWindow.ui.Quitar.clicked.connect(self.funcion_b2)#listener del boton parar
         self.MainWindow.ui.cargar_imagen.clicked.connect(self.cargar_imagen)
         self.MainWindow.ui.original.clicked.connect(self.original)
         self.MainWindow.ui.filtro1.clicked.connect(self.filtro1)
@@ -40,7 +40,6 @@ class App(QtWidgets.QApplication):
     # metodos
 
     def funcion_b1(self):#funcion del boton iniciar
-        print ("Iniciar")
         if(self.path!=''):
             self.sosp += 1 #elemento variable
             self.MainWindow.ui.LCDnumber.display(self.sosp)#cambia el numero LCD
@@ -52,57 +51,72 @@ class App(QtWidgets.QApplication):
             self.MainWindow.ui.filtro2.setEnabled(True)
             self.MainWindow.ui.filtro3.setEnabled(True)
         else:
-            print ('No se cargo imagen')
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada para analizar")
 
-    def funcion_b2(self):#funcion del boton parar
-        print ("Parar")
-    
+    def funcion_b2(self):#funcion del boton quitar
+        if(self.path!=''):
+            self.path = ''
+            self.MainWindow.ui.imagen_an.setText('Agregar imagen a analizar')
+            QtWidgets.QMessageBox.about(self.MainWindow, " ","Se quito imagen analizada")
+        else:
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada")
 
-    def cargar_imagen(self):
+    def cargar_imagen(self):#funcion del boton cargar imagen
         self.path,m = QtWidgets.QFileDialog.getOpenFileName(self.MainWindow, 'Abrir Imagen',"","Image files (*.jpg *.png)")
         if(self.path!= ''):
             pixmap = QtGui.QPixmap(self.path)
             pixmap = pixmap.scaled(600, 400)
             self.MainWindow.ui.imagen_an.setPixmap(pixmap)
         else:
-            print ('No se cargo imagen')
-        
+            QtWidgets.QMessageBox.about(self.MainWindow, " ","No selecciono ninguna imagen para cargar")
 
     def original(self):
-        pixmap = QtGui.QPixmap(self.path)
-        pixmap = pixmap.scaled(600, 400)
-        self.MainWindow.ui.imagen_an.setPixmap(pixmap)
-        self.MainWindow.ui.original.setEnabled(False)
-        self.MainWindow.ui.filtro1.setEnabled(True)
-        self.MainWindow.ui.filtro2.setEnabled(True)
-        self.MainWindow.ui.filtro3.setEnabled(True)
+        if(self.path!= ''):
+            pixmap = QtGui.QPixmap(self.path)
+            pixmap = pixmap.scaled(600, 400)
+            self.MainWindow.ui.imagen_an.setPixmap(pixmap)
+            self.MainWindow.ui.original.setEnabled(False)
+            self.MainWindow.ui.filtro1.setEnabled(True)
+            self.MainWindow.ui.filtro2.setEnabled(True)
+            self.MainWindow.ui.filtro3.setEnabled(True)
+        else:
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada")
     
     def filtro1(self):
-        pixmap = QtGui.QPixmap(self.path)
-        pixmap = pixmap.scaled(600, 400)
-        self.MainWindow.ui.imagen_an.setPixmap(pixmap)
-        self.MainWindow.ui.filtro1.setEnabled(False)
-        self.MainWindow.ui.original.setEnabled(True)
-        self.MainWindow.ui.filtro2.setEnabled(True)
-        self.MainWindow.ui.filtro3.setEnabled(True)
+        if(self.path!= ''):    
+            pixmap = QtGui.QPixmap(self.path)
+            pixmap = pixmap.scaled(600, 400)
+            self.MainWindow.ui.imagen_an.setPixmap(pixmap)
+            self.MainWindow.ui.filtro1.setEnabled(False)
+            self.MainWindow.ui.original.setEnabled(True)
+            self.MainWindow.ui.filtro2.setEnabled(True)
+            self.MainWindow.ui.filtro3.setEnabled(True)
+        else:
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada")
 
     def filtro2(self):
-        pixmap = QtGui.QPixmap(self.path)
-        pixmap = pixmap.scaled(600, 400)
-        self.MainWindow.ui.imagen_an.setPixmap(pixmap)
-        self.MainWindow.ui.filtro2.setEnabled(False)
-        self.MainWindow.ui.original.setEnabled(True)
-        self.MainWindow.ui.filtro1.setEnabled(True)
-        self.MainWindow.ui.filtro3.setEnabled(True)
+        if(self.path!= ''):
+            pixmap = QtGui.QPixmap(self.path)
+            pixmap = pixmap.scaled(600, 400)
+            self.MainWindow.ui.imagen_an.setPixmap(pixmap)
+            self.MainWindow.ui.filtro2.setEnabled(False)
+            self.MainWindow.ui.original.setEnabled(True)
+            self.MainWindow.ui.filtro1.setEnabled(True)
+            self.MainWindow.ui.filtro3.setEnabled(True)
+        else:
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada")
 
     def filtro3(self):
-        pixmap = QtGui.QPixmap(self.path)
-        pixmap = pixmap.scaled(600, 400)
-        self.MainWindow.ui.imagen_an.setPixmap(pixmap)
-        self.MainWindow.ui.filtro3.setEnabled(False)
-        self.MainWindow.ui.original.setEnabled(True)
-        self.MainWindow.ui.filtro2.setEnabled(True)
-        self.MainWindow.ui.filtro1.setEnabled(True)
+        if(self.path!= ''):
+            pixmap = QtGui.QPixmap(self.path)
+            pixmap = pixmap.scaled(600, 400)
+            self.MainWindow.ui.imagen_an.setPixmap(pixmap)
+            self.MainWindow.ui.filtro3.setEnabled(False)
+            self.MainWindow.ui.original.setEnabled(True)
+            self.MainWindow.ui.filtro2.setEnabled(True)
+            self.MainWindow.ui.filtro1.setEnabled(True)
+        else:
+            QtWidgets.QMessageBox.about(self.MainWindow, "Error","No hay imagen cargada")
                 
 ## Se crea una funcion que cree un objeto aplicacion, entregandole argumentos de sistema.
 def main(args):
