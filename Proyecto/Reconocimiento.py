d07be2c4fb17ecfbc640616657771a56acb5e426
konata2117 <natashasmile11@gmail.com>
2018-11-25 20:31:42 -0300

    25/11

--- Proyecto/Reconocimiento.py
+++ Proyecto/Reconocimiento.py
@@ -2,78 +2,73 @@ from PIL import Image
 import numpy as np
 import cv2
 import scipy
-from skimage import filters
+#from skimage import filters
 import matplotlib.pyplot as plt
 class Detector:
     def __init__(self,nombre_archivo):
         self.nombre_archivo = nombre_archivo
         # Cargamos la imagen
         self.original = cv2.imread(self.nombre_archivo)
-        #self.original =cv2.medianBlur(self.original,5)
+        #transformamos a escala de grises
         self.gris = cv2.cvtColor(self.original, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow("original",self.original)
+        #variables que se usan mas adelante
         self.mygris=None
         self.cont = 0
         self.trans2 = ''
 
     def transformacion1(self):    
-        gris1=cv2.equalizeHist(self.gris)
+        #transformacion a rgb
+        gris1=cv2.cvtColor(self.original, cv2.COLOR_BGR2RGB)
+        #transformacion a otra escala de grises para cuerpos que estan muy juntos
+        #gris1=cv2.equalizeHist(self.gris)
+        #hacerla igual a otra imagen para poder llamarla desde afuera de la funcion 
         self.mygris=gris1
-        #cv2.imshow("imagen",gris1)
-
-        #ret,th1 = cv2.threshold(self.gris,127,255,cv2.THRESH_BINARY)
-        #cv2.imshow("th1", th1)
+        #muestra la imagen 
+        #cv2.imshow("imagen",gris1) 
+        #convierte a imagen la matriz que se creo de la transformacion
         img_th1 = Image.fromarray(gris1)
-        #cv2.waitKey(0)
+        #retorna la imagen para la interfaz    
         return img_th1
         
         
     def transformacion2(self):
-        trans = ''
-        trans = cv2.adaptiveThreshold(self.mygris,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+        #transforma la imagen en una especie de dibujo
+        trans = cv2.adaptiveThreshold(self.gris,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
         cv2.THRESH_BINARY,11,2)
-        #kernel = np.ones((3,3),np.uint8)
-        #trans= cv2.morphologyEx(th3,cv2.MORPH_CLOSE,kernel)
-        #img_th3 = Image.fromarray(th3)
+        #transforma la matriz entregado de la transformacion anterior a imagen
         img_trans = Image.fromarray(trans)
-        #img_trans.save('img_trans.png')
-        #img_trans.show()
-        #cv2.imshow("th3", th3)
-        #cv2.imshow("trans", trans)
-        #print type(cv2)
-        #cv2.waitKey(0)
+        #se iguala la matrizz obtenida a una global, para llamarla desde otra funcion
         self.trans2 = trans
+        #se retorna la imagen para que la interfaz la muestre
         return img_trans        
         
         
     def bordes(self):
+        #llamamos desde la funcion tranformacion2 la matriz entregada
         trans = self.trans2
-        # Convertimos a escala de grises
-        #gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
- 
-        # Aplicar suavizado Gaussiano
-        #gauss = cv2.GaussianBlur(gris, (5,5), 0)
- 
-        #cv2.imshow("suavizado", gauss)
+       
  
         # Detectamos los bordes con Canny
         canny = cv2.Canny(self.gris, 10, 100,3)
+        #eliminamos el ruido , o manchas negras de mas en la foto 
         kernel = np.ones((3,3),np.uint8)
         canny= cv2.morphologyEx(canny,cv2.MORPH_CLOSE,kernel)
-        
-        #cv2.imshow("canny", canny)
  
         # Buscamos los contornos
-        (_, contornos,_) = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        self.cont = format(len(contornos)-1)
+        (contornos,_) = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.cont = format(len(contornos))
         # Mostramos el numero de objetos por consola
-        #print ("He encontrado {} objetos".format(len(contornos)-1))
+        #print ("He encontrado {} objetos".format(len(contornos)))
+        #dibujamos los contornos encontrados en la imagen original
         cv2.drawContours(self.original,contornos,-1,(0,0,255), 2)
         #cv2.imshow("contornos", self.original)
+        #se transforma la matriz a imagen 
         img_cont = Image.fromarray(self.original)
+        #se retorna la imagen para la interfaz
         return img_cont
 
     def num_objetos(self):
+        #retorna el numero de objetos encontrados por las transformaciones correspondientes 
         return int(self.cont)
 
         
